708b997eaf54b0fc369fc292708dfa43afef0e71
SaraabbasiNZ <>
2023-11-26 00:51:22 +0000

    Add five JS questions with answers

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        question: "Javascript is an _______ language?",
+        options: ["Object-Oriented", "Object-Based", "Procedural", "None of them"],
+        correctAnswer: "Object-Oriented"
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        options: ["var", "let", "Both A and B", "None of them"],
+        correctAnswer: "Both A and B"
+    },
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        options: ["getElementById()", "getElementByClassName()", "Both A and B", "None of them"],
+        correctAnswer: "Both A and B"
+    },
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        options: ["Throw an error", "Ignores the statements", "Gives a warning", "None of them"],
+        correctAnswer: "Ignores the statements"
+    },
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        options: ["document.write()", "console.log()", "windows.alert()", "All of them"],
+        correctAnswer: "All of them"
+    }
+
+    
+];
